705f2fd9433d031c2c5fc67c3e6e6b2754f137f8
dkhomutov <dmitry.khomutov@infobip.com>
2017-09-04 11:38:21 +0300

    apply function

---
+++ src/main/java/org/angry1980/security/jwt/JwtSecurityAutoConfiguration.kt
@@ -0,0 +1,33 @@
+package org.angry1980.security.jwt
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Scope
+import org.springframework.security.config.web.server.HttpJwtSecurity
+import org.springframework.security.web.server.context.ServerWebExchangeAttributeSecurityContextRepositoryFix
+
+import org.springframework.security.config.web.server.HttpJwtSecurity.jwt
+
+/**
+ * Auto-configuration for JSON Web Token authentication method.
+ */
+@Configuration
+@ConditionalOnProperty(name = arrayOf("security.jwt.signingKey"), matchIfMissing = false)
+class JwtSecurityAutoConfiguration {
+
+    /**
+     * Prototype bean of Jwt Security builder which later can be used to create web filter chains.
+     */
+    @Bean
+    @Scope("prototype")
+    fun jwtSecurity(@Value("security.jwt.signingKey") signingKey: String) = jwt().apply {
+        signingKey(signingKey)
+        //todo: delete after spring fix
+        securityContextRepository(ServerWebExchangeAttributeSecurityContextRepositoryFix())
+        //security.securityContextRepository(new ServerWebExchangeAttributeSecurityContextRepository());
+    }
+
+
+}
